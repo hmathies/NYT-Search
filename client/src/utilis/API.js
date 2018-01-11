120a981390ccbad173bf2d26b02deaed9053e276
Heather <hlmathies@gmail.com>
2018-01-11 14:40:06 -0500

    Delete API.js functions

--- client/src/utilis/API.js
+++ client/src/utilis/API.js
@@ -1,31 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all articles
   getArticles: function() {
-    return axios.get("/api/articles");
+    return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3665355e36be4471abd55f1fb42c279b&query=' + query");
   },
-  // Gets the book with the given id
+  // Gets the article with the given id
   getArticle: function(id) {
     return axios.get("/api/articles/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the article with the given id
   deleteArticle: function(id) {
     return axios.delete("/api/articles/" + id);
   },
-  // Saves a book to the database
+  // Saves a article to the database
   saveArticle: function(articleData) {
     return axios.post("/api/articles", articleData);
   }
 };
-
-// request.get({
-//   url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
-//   qs: {
-//     'api-key': "3665355e36be4471abd55f1fb42c279b",
-//     'begin_date': "20140101"
-//   },
-// }, function(err, response, body) {
-//   body = JSON.parse(body);
-//   console.log(body);
-// })
